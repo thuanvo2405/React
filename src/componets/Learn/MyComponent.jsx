d5ab59876a7af4eee5b877685e33854219e33ee4
ThuanVo <vothuan2405003@gmail.com>
2024-11-09 12:54:54 +0700

    Video 30

--- src/componets/Learn/MyComponent.jsx
+++ src/componets/Learn/MyComponent.jsx
@@ -1,5 +1,12 @@
+import "./styles.css";
+
 const MyComponent = () => {
-    return (<h2>Video #28</h2>);
+    return (
+        <>
+            <h2>Video #28</h2>
+            <h2 className="child" style={{ borderRadius: "8px" }}>CSS Format</h2 >
+        </>
+    );
 }
 
 export default MyComponent;
