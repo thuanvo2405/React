d2e2494b9550da3023b8f5bef3dcf036fc094d14
ThuanVo <vothuan2405003@gmail.com>
2024-11-16 14:35:31 +0700

    VIDEO 73

--- src/componets/user/user_table.jsx
+++ src/componets/user/user_table.jsx
@@ -1,75 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Space, Table, Tag } from 'antd';
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-    },
-    {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, { tags }) => (
-            <>
-                {tags.map((tag) => {
-                    let color = tag.length > 5 ? 'geekblue' : 'green';
-                    if (tag === 'loser') {
-                        color = 'volcano';
-                    }
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </>
-        ),
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <a>Invite {record.name}</a>
-                <a>Delete</a>
-            </Space>
-        ),
-    },
-];
-const data = [
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        tags: ['loser'],
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-    },
-];
-const UserTable = () => <Table columns={columns} dataSource={data} />;
+import { fetchAllUser } from '../../service/api.service';
+
+const UserTable = () => {
+    const [dataUsers, setDataUsers] = useState([]);
+
+    useEffect(() => {
+        loadUser();
+    }, []);
+    const columns = [
+        {
+            title: 'Id',
+            dataIndex: '_id',
+        },
+        {
+            title: 'Full name',
+            dataIndex: 'full name',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+        },
+        {
+            title: 'Phone Number',
+            dataIndex: 'phone',
+        },
+    ];
+
+    const loadUser = async () => {
+        const res = await fetchAllUser();
+        setDataUsers(res.data);
+    }
+    return <Table columns={columns} dataSource={dataUsers} rowKey={"_id"} />
+};
 export default UserTable;
--- src/service/api.service.js
+++ src/service/api.service.js
@@ -15,7 +15,14 @@ const createUserAPI = (fullName, email, password, phone) => {
     return axios.post(URL_BACKEND, data, config);
 }
 
+const fetchAllUser = () => {
+    const URL_BACKEND = "/api/v1/user";
+    const config = { headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsInBob25lIjoiMTIzNDU2Nzg5IiwiZnVsbE5hbWUiOiJJJ20gQWRtaW4iLCJyb2xlIjoiQURNSU4iLCJzdWIiOiI2NzMyMTY2Yzc1YTVjODcyNGIxZmIwNTYiLCJhdmF0YXIiOiIyMTIzMmYyOTdhNTdhNWE3NDM4OTRhMGU0YTgwMWZjMy5wbmciLCJpYXQiOjE3MzE3MjYwMjgsImV4cCI6MTczMTc2MjAyOH0.X-DAaiWGGJDnVYZBiMsSbsFw8UQ1q-ut8BoLywQLieo` } };
+    return axios.get(URL_BACKEND, config);
+}
+
 export {
     createUserAPI,
-    updateUserAPI
+    updateUserAPI,
+    fetchAllUser
 }
